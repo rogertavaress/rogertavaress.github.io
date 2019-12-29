36a93be1c5e36225aa071b5ef0ce8e4f7a5ff03e
Rogério Tavares <rogerioctf@gmail.com>
2019-12-29 20:32:36 -0300

    add comandHelp

--- js/index.js
+++ js/index.js
@@ -1,23 +1,3 @@
-/*
- ______       # ______      # _______     # ______      # ______       # ________     # ______      #
-/_____/\      #/_____/\     #/______/\    #/_____/\     #/_____/\      #/_______/\    #/_____/\     #
-\:::_ \ \     #\:::_ \ \    #\::::__\/__  #\::::_\/_    #\:::_ \ \     #\__.::._\/    #\:::_ \ \    #
- \:(_) ) )_   # \:\ \ \ \   # \:\ /____/\ # \:\/___/\   # \:(_) ) )_   #   \::\ \     # \:\ \ \ \   #
-  \: __ `\ \  #  \:\ \ \ \  #  \:\\_  _\/ #  \::___\/_  #  \: __ `\ \  #   _\::\ \__  #  \:\ \ \ \  #
-   \ \ `\ \ \ #   \:\_\ \ \ #   \:\_\ \ \ #   \:\____/\ #   \ \ `\ \ \ #  /__\::\__/\ #   \:\_\ \ \ #
-    \_\/ \_\/ #    \_____\/ #    \_____\/ #    \_____\/ #    \_\/ \_\/ #  \________\/ #    \_____\/ #
-              ##             ##             ##             ##              ##              ##         
-
-
-.______        ______     _______  _______ .______       __    ______   
-|   _  \      /  __  \   /  _____||   ____||   _  \     |  |  /  __  \  
-|  |_)  |    |  |  |  | |  |  __  |  |__   |  |_)  |    |  | |  |  |  | 
-|      /     |  |  |  | |  | |_ | |   __|  |      /     |  | |  |  |  | 
-|  |\  \----.|  `--'  | |  |__| | |  |____ |  |\  \----.|  | |  `--'  | 
-| _| `._____| \______/   \______| |_______|| _| `._____||__|  \______/  
-
-*/
-
 const janela = document.getElementsByClassName("janela")[0];
 let comandInputs = document.getElementsByClassName("comandInput");
 console.log(janela);
@@ -30,39 +10,115 @@ function ultimoInput() {
 
 function desabilitarInputs() {
   comandInputs = document.getElementsByClassName("comandInput");
-  if (true) {
+  if (comandInputs.length > 1) {
     for (comandInput of comandInputs) {
       comandInput.disabled = true;
     }
-    ultimoInput().disabled = false;
   }
+  ultimoInput().disabled = false;
+  ultimoInput().focus();
 }
 
 comandInput.addEventListener("keydown", function(e) {
   if (e.keyCode === 13) {
-    outputComand(comandInput.value);
+    outputComand(comandInput.value.toUpperCase());
   }
 });
 
 function gerarComand() {
-  const comandHtml =
-    '<div class="comand"><span class="primaryColor">terminalLinux@user</span><span>:</span><span class="secondaryColor">~/</span><span>$ </span><input type="text" id="comandInput" class="comandInput" maxlength="15" autofocus /></div>';
-  return comandHtml;
+  const comand = document.createElement("div");
+  comand.setAttribute("class", "comand");
+
+  const parte1 = document.createElement("span");
+  parte1.setAttribute("class", "primaryColor");
+  parte1.appendChild(document.createTextNode("terminalLinux@user"));
+
+  const parte2 = document.createElement("span");
+  parte2.appendChild(document.createTextNode(":"));
+
+  const parte3 = document.createElement("span");
+  parte3.setAttribute("class", "secondaryColor");
+  parte3.appendChild(document.createTextNode("~/"));
+
+  const parte4 = document.createElement("span");
+  parte4.appendChild(document.createTextNode("$ "));
+
+  const input = document.createElement("input");
+  input.setAttribute("type", "text");
+  input.setAttribute("class", "comandInput");
+  input.setAttribute("maxlength", "12");
+  input.addEventListener("keydown", function(e) {
+    if (e.keyCode === 13) {
+      outputComand(ultimoInput().value.toUpperCase());
+    }
+  });
+
+  comand.appendChild(parte1);
+  comand.appendChild(parte2);
+  comand.appendChild(parte3);
+  comand.appendChild(parte4);
+  comand.appendChild(input);
+
+  return comand;
 }
 
 function outputComand(comand) {
-  if (comand === "help") {
+  if (comand === "HELP") {
     console.log("Ajuda!");
     comandHelp();
+    janela.appendChild(gerarComand());
+    desabilitarInputs();
+  } else if (comand === "CLEAR") {
+    console.log("Limpar!");
+    comandClear();
+    janela.appendChild(gerarComand());
   }
 }
 
 function comandHelp() {
   const area = document.createElement("div");
-  const title = document.createElement("h2");
-  title.appendChild(document.createTextNode("Help"));
+  const title = document.createElement("img");
+  title.setAttribute("src", "./img/help.svg");
+  title.setAttribute("alt", "Help");
+  title.setAttribute("class", "title");
+  const list = document.createElement("p");
+  list.appendChild(document.createTextNode("Lista de Comandos:"));
+  list.appendChild(document.createElement("br"));
+  list.appendChild(document.createElement("br"));
+  list.appendChild(
+    document.createTextNode("- Help -> Listar todos os comandos.")
+  );
+  list.appendChild(document.createElement("br"));
+  list.appendChild(
+    document.createTextNode("- About -> Mostrar algumas informações sobre mim.")
+  );
+  list.appendChild(document.createElement("br"));
+  list.appendChild(
+    document.createTextNode(
+      "- Experiences -> Listar todas as minhas experiências profissionais."
+    )
+  );
+  list.appendChild(document.createElement("br"));
+  list.appendChild(
+    document.createTextNode(
+      "- Skills -> Mostrar todas as tecnologias que estudo."
+    )
+  );
+  list.appendChild(document.createElement("br"));
+  list.appendChild(
+    document.createTextNode("- Contact -> Mostrar onde você pode me encontrar.")
+  );
+  list.appendChild(document.createElement("br"));
+  list.appendChild(document.createTextNode("- Clear -> Limpar a tela."));
 
   area.appendChild(title);
+  area.appendChild(list);
 
   janela.appendChild(area);
+}
+
+function comandAbout() {}
+
+function comandClear() {
+  janela.innerHTML = "";
 }
