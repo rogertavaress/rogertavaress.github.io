23e07be7fb03138a7c1836af158c247d4359cf91
Rogério Tavares <rogerioctf@gmail.com>
2019-12-30 12:00:49 -0300

    add função desktop

---
+++ js/desktop.js
@@ -0,0 +1,37 @@
+function maximizar() {
+  console.log("maximizar");
+  const body = document.getElementsByTagName("body")[0];
+  const janela = document.getElementsByClassName("janela")[0];
+  const header = document.getElementsByTagName("header")[0];
+
+  const botaoA = document.getElementById("btA");
+  const botaoImg = document.getElementById("btImg");
+
+  body.style.backgroundImage = "";
+  body.style.backgroundColor = "rgb(41, 39, 54)";
+  janela.style.display = "block";
+
+  botaoA.onclick = minimizar;
+  botaoImg.src = "./img/btMinimizar.svg";
+
+  header.setAttribute("class", "barratituloMax");
+}
+
+function minimizar() {
+  console.log("minimizar");
+  const body = document.getElementsByTagName("body")[0];
+  const janela = document.getElementsByClassName("janela")[0];
+  const header = document.getElementsByTagName("header")[0];
+
+  const botaoA = document.getElementById("btA");
+  const botaoImg = document.getElementById("btImg");
+
+  body.style.backgroundImage = "url('./img/wallpaper.jpg')";
+  body.style.backgroundSize = "cover";
+  janela.style.display = "none";
+
+  botaoA.onclick = maximizar;
+  botaoImg.src = "./img/btMaximizar.svg";
+
+  header.setAttribute("class", "barratituloMin");
+}
