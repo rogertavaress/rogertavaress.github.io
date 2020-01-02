f8af23c6d8a8f28ef60a43c9437ec8f1a67ed202
Rogério Tavares <rogerioctf@gmail.com>
2020-01-01 21:05:17 -0300

    Ajuste posição img

--- js/desktop.js
+++ js/desktop.js
@@ -3,13 +3,14 @@ function maximizar() {
   const body = document.getElementsByTagName("body")[0];
   const janela = document.getElementsByClassName("janela")[0];
   const header = document.getElementsByTagName("header")[0];
+  const backgroundImg = document.getElementsByClassName("bg")[0];
 
   const botaoA = document.getElementById("btA");
   const botaoImg = document.getElementById("btImg");
 
-  body.style.backgroundImage = "";
   body.style.backgroundColor = "rgb(41, 39, 54)";
   janela.style.display = "block";
+  backgroundImg.style.display = "none";
 
   botaoA.onclick = minimizar;
   botaoImg.src = "./img/btMinimizar.svg";
@@ -22,17 +23,15 @@ function minimizar() {
   const body = document.getElementsByTagName("body")[0];
   const janela = document.getElementsByClassName("janela")[0];
   const header = document.getElementsByTagName("header")[0];
+  const backgroundImg = document.getElementsByClassName("bg")[0];
+
+  body.style.backgroundColor = "transparent";
 
   const botaoA = document.getElementById("btA");
   const botaoImg = document.getElementById("btImg");
 
-  body.style.backgroundImage = "url('./img/wallpaper.jpg')";
-  body.style.backgroundRepeat = "no-repeat";
-  body.style.webkitBackgroundSize = "cover";
-  body.style.backgroundSize = "cover";
-  body.style.backgroundAttachment = "fixed";
-
   janela.style.display = "none";
+  backgroundImg.style.display = "block";
 
   botaoA.onclick = maximizar;
   botaoImg.src = "./img/btMaximizar.svg";
